ec7ed7791c3a98022dac5f2bf2af11d80579a8fc
Miguel Fernandez <miguelfdez99@gmail.com>
2023-03-29 22:37:37 +0200

    auto generated

--- frontend/wailsjs/go/main/App.js
+++ frontend/wailsjs/go/main/App.js
@@ -22,6 +22,10 @@ export function DeleteUserWithDir(arg1) {
   return window['go']['main']['App']['DeleteUserWithDir'](arg1);
 }
 
+export function GetCPUInfo() {
+  return window['go']['main']['App']['GetCPUInfo']();
+}
+
 export function GetDistribution() {
   return window['go']['main']['App']['GetDistribution']();
 }
