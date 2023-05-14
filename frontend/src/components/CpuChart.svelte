a8fbf82d5408df6f87e55d953171e7878e264de2
Miguel Fernandez <miguelfdez99@gmail.com>
2023-05-14 17:26:02 +0200

    Improvements in charts

--- frontend/src/components/CpuChart.svelte
+++ frontend/src/components/CpuChart.svelte
@@ -18,12 +18,12 @@
         LineElement
     );
 
-    let chart;
-    let cpuUsages = [];
-    let intervalId;
-    let canvas;
+    let chart: Chart;
+    let intervalId: number;
+    let canvas: HTMLCanvasElement;
 
-    const MAX_HISTORY = 60; // Keep data for the last 60 seconds
+
+    const MAX_HISTORY = 300; // Keep data for the last 60 seconds
     const COLORS = [
         "red",
         "blue",
@@ -44,32 +44,42 @@
     ];
 
     onMount(async () => {
-        cpuUsages = await GetCPUUsage();
-
         const ctx = canvas.getContext("2d");
         chart = new Chart(ctx, {
             type: "line",
             data: {
-                labels: ["", ...Array(MAX_HISTORY - 1).fill("")],
-                datasets: cpuUsages.map((usage, i) => ({
-                    label: `CPU ${i + 1} (${usage}%)`,
-                    data: [null, ...Array(MAX_HISTORY - 1).fill(usage)],
-                    borderColor: COLORS[i % COLORS.length],
+                labels: Array(MAX_HISTORY).fill(""),
+                datasets: COLORS.map((color, i) => ({
+                    label: `CPU ${i + 1}`,
+                    data: Array(MAX_HISTORY).fill(null),
+                    borderColor: color,
                     borderWidth: 1,
                     fill: false,
                     tension: 0.1,
+                    pointRadius: 0,
                 })),
             },
             options: {
+                animation: false,
                 scales: {
                     y: {
                         beginAtZero: true,
+                        min: 0,
+                        max: 100,
+                        ticks: {
+                            stepSize: 20,
+                        },
+                    },
+                },
+                plugins: {
+                    tooltip: {
+                        enabled: true,
                     },
                 },
             },
         });
 
-        intervalId = setInterval(updateChart, 1000);
+        intervalId = setInterval(updateChart, 100);
     });
 
     onDestroy(() => {
@@ -80,16 +90,10 @@
         const newCpuUsages = await GetCPUUsage();
 
         newCpuUsages.forEach((usage, i) => {
+            chart.data.datasets[i].data.shift();
             chart.data.datasets[i].data.push(usage);
-
             const roundedPercentage = usage.toFixed(1);
-            chart.data.datasets[i].label = `CPU ${
-                i + 1
-            } (${roundedPercentage}%)`;
-
-            if (chart.data.datasets[i].data.length > MAX_HISTORY) {
-                chart.data.datasets[i].data.shift();
-            }
+            chart.data.datasets[i].label = `CPU ${i + 1} (${roundedPercentage}%)`;
         });
 
         chart.update();
@@ -97,3 +101,10 @@
 </script>
 
 <canvas bind:this={canvas} />
+
+<style>
+    canvas {
+        width: 100%;
+        height: 100%;
+    }
+</style>
