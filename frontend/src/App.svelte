12479cb2f546ebb6303965f949bc1398d92b32ee
Miguel Fernandez <miguelfdez99@gmail.com>
2023-06-18 00:42:33 +0200

    change importation

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,15 +1,6 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import Home from './views/Home.svelte';
-  import Users from './views/Users.svelte';
-  import Packages from './views/Packages.svelte';
-  import Monitor from './views/Monitoring.svelte';
-  import Cron from './views/Cron.svelte';
-  import Firewall from './views/Firewall.svelte';
-  import Backup from './views/Backup.svelte';
-  import Process from './views/ProcInfo.svelte';
-  import Networking from './views/Networking.svelte';
-  import Logs from './views/Logs.svelte';
+  import { Home, Users, Packages, Monitor, Cron, Firewall, Backup, Process, Networking, Logs } from './imports';
   import "@picocss/pico/css/pico.css"
   let currentView = 'home';
 
