e3fe12a856b71cb1608e11e8fdde198ac990b0a0
Miguel Fernandez <miguelfdez99@gmail.com>
2023-06-18 00:42:18 +0200

    created file that has views imports

---
+++ frontend/src/imports.ts
@@ -0,0 +1,12 @@
+import Home from './views/Home.svelte';
+import Users from './views/Users.svelte';
+import Packages from './views/Packages.svelte';
+import Monitor from './views/Monitoring.svelte';
+import Cron from './views/Cron.svelte';
+import Firewall from './views/Firewall.svelte';
+import Backup from './views/Backup.svelte';
+import Process from './views/ProcInfo.svelte';
+import Networking from './views/Networking.svelte';
+import Logs from './views/Logs.svelte';
+
+export { Home, Users, Packages, Monitor, Cron, Firewall, Backup, Process, Networking, Logs };
