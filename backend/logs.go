eae683a54dea4bce262408b7159904addbc503dd
Miguel Fernandez <miguelfdez99@gmail.com>
2023-06-18 00:41:43 +0200

    Improve function

--- backend/logs.go
+++ backend/logs.go
@@ -3,18 +3,24 @@ package backend
 import (
 	"bufio"
 	"fmt"
+	"os"
 	"os/exec"
 )
 
-func (b *Backend) GetLogs(logType string) ([]string, error) {
+func (b *Backend) GetLogs(logType string, boot int) ([]string, error) {
+	bootFlag := fmt.Sprintf("-b %d", boot)
 	var command *exec.Cmd
 	switch logType {
 	case "all":
-		command = exec.Command("journalctl", "--no-pager")
+		command = exec.Command("journalctl", "--no-pager", bootFlag)
 	case "system":
-		command = exec.Command("journalctl", "--no-pager", "-k")
+		command = exec.Command("journalctl", "--no-pager", "-k", bootFlag)
+	case "hardware":
+		command = exec.Command("sh", "-c", "journalctl --no-pager -k "+bootFlag+" | grep -iE 'usb|pci|hdmi|hid|eth|wlan|bluetooth'")
 	case "application":
-		command = exec.Command("journalctl", "--no-pager")
+		command = exec.Command("sh", "-c", fmt.Sprintf("journalctl --no-pager %s | grep -v 'kernel\\|systemd'", bootFlag))
+	case "important":
+		command = exec.Command("journalctl", "--no-pager", "-p", "err", bootFlag)
 	default:
 		return nil, fmt.Errorf("unknown log type: %s", logType)
 	}
@@ -49,4 +55,41 @@ func (b *Backend) GetLogs(logType string) ([]string, error) {
 
 	//fmt.Println(logLines)
 	return logLines, nil
+}
+
+func (b *Backend) ExportLogs(logType string, filepath string) error {
+	var command *exec.Cmd
+
+	switch logType {
+	case "all":
+		command = exec.Command("journalctl", "--no-pager")
+	case "system":
+		command = exec.Command("journalctl", "--no-pager", "-k")
+	case "hardware":
+		command = exec.Command("journalctl", "--no-pager", "_SYSLOG_IDENTIFIER=kernel")
+	case "application":
+		command = exec.Command("journalctl", "--no-pager")
+	case "important":
+		command = exec.Command("journalctl", "--no-pager", "-p", "err")
+	default:
+		return nil
+	}
+
+	file, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	command.Stdout = file
+
+	if err := command.Start(); err != nil {
+		return err
+	}
+
+	if err := command.Wait(); err != nil {
+		return err
+	}
+
+	return nil
 }
