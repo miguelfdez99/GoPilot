81869726af16b6e9834345f322901ba7018c1fe3
Miguel Fernandez <miguelfdez99@gmail.com>
2023-07-28 19:24:25 +0200

    Added image icon to open dialogs

--- frontend/src/views/Alerts.svelte
+++ frontend/src/views/Alerts.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-    import { onMount, onDestroy } from 'svelte';
-    import { writable } from 'svelte/store';
+    import { onMount, onDestroy } from "svelte";
+    import { writable } from "svelte/store";
     import {
         MonitorFile,
         MonitorDir,
         WatchList,
         MonitorSystemStats,
+        OpenFile,
+        OpenDir,
     } from "../../wailsjs/go/backend/Backend";
+    import openIcon from "../assets/images/open.png";
 
     interface WatchItem {
         path: string;
@@ -51,50 +54,87 @@
 
     async function fetchWatchList() {
         const newWatchList: { [key: string]: string } = await WatchList();
-        const transformedWatchList: Array<WatchItem> = Object.entries(newWatchList).map(([path, type]) => ({ path, type }));
+        const transformedWatchList: Array<WatchItem> = Object.entries(
+            newWatchList
+        ).map(([path, type]) => ({ path, type }));
         watchListStore.set(transformedWatchList);
     }
 
+    const selectFile = async () => {
+        const file = await OpenFile();
+        filepath = file;
+    };
+
+    const selectDir = async () => {
+        const dir = await OpenDir();
+        dirpath = dir;
+    };
 </script>
 
-
 <div class="monitor-container">
-
     <div class="monitor-section">
         <h1>File System Monitor</h1>
 
         <h2>File Monitor</h2>
         <div class="input-group">
-            <input type="text" bind:value={filepath} placeholder="Enter a file path" />
-            <button on:click={startMonitoringFile}>Start Monitoring File</button>
+            <input
+                type="text"
+                bind:value={filepath}
+                placeholder="Enter a file path"
+            />
+            <button class="open-btn" title="Choose File" on:click={selectFile}
+                ><img src={openIcon} alt="Open Dir" class="open-icon" /></button
+            >
         </div>
+        <button on:click={startMonitoringFile}>Start Monitoring File</button
+            >
 
         <h2>Directory Monitor</h2>
         <div class="input-group">
-            <input type="text" bind:value={dirpath} placeholder="Enter a directory path" />
-            <button on:click={startMonitoringDir}>Start Monitoring Directory</button>
+            <input
+                type="text"
+                bind:value={dirpath}
+                placeholder="Enter a directory path"
+            />
+            <button class="open-btn" title="Choose Directory" on:click={selectDir}
+                ><img src={openIcon} alt="Open Dir" class="open-icon" /></button
+            >
         </div>
+        <button on:click={startMonitoringDir}
+                >Start Monitoring Directory</button
+            >
     </div>
 
     <div class="monitor-section">
         <h2>System Stats Monitor</h2>
         <div class="stats-group">
-            <input type="number" bind:value={cpuThreshold} placeholder="Enter CPU threshold (in %)" />
-            <input type="number" bind:value={ramThreshold} placeholder="Enter RAM threshold (in GB)" />
-            <input type="number" bind:value={diskThreshold} placeholder="Enter Disk Space threshold (in GB)" />
+            <input
+                type="number"
+                bind:value={cpuThreshold}
+                placeholder="Enter CPU threshold (in %)"
+            />
+            <input
+                type="number"
+                bind:value={ramThreshold}
+                placeholder="Enter RAM threshold (in GB)"
+            />
+            <input
+                type="number"
+                bind:value={diskThreshold}
+                placeholder="Enter Disk Space threshold (in GB)"
+            />
         </div>
-        <button on:click={startMonitoringSystemStats}>Start Monitoring System Stats</button>
-    
+        <button on:click={startMonitoringSystemStats}
+            >Start Monitoring System Stats</button
+        >
+
         <h2>Watch List</h2>
         <ul>
             {#each $watchListStore as { path, type }}
                 <li><strong>{type}</strong>: {path}</li>
             {/each}
         </ul>
-        
-        
     </div>
-
 </div>
 
 <style>
@@ -114,20 +154,22 @@
         color: #ddd;
         margin: 1rem;
     }
-    
-    h1, h2 {
+
+    h1,
+    h2 {
         color: #ddd;
         margin-bottom: 1em;
     }
 
     .input-group {
         display: flex;
-        flex-direction: column;
+        flex-direction: row;
+        align-items: center;
         margin-bottom: 1em;
     }
 
     input {
-        padding: .7em;
+        padding: 0.7em;
         border: 0;
         border-radius: 4px;
         background: #333;
@@ -138,7 +180,7 @@
     }
 
     button {
-        padding: .8em 1em;
+        padding: 0.8em 1em;
         border: none;
         border-radius: 4px;
         background: #1abc9c;
@@ -153,6 +195,24 @@
         background-color: #16a085;
     }
 
+    button.open-btn {
+        background: none;
+        width: 2.5rem;
+        height: 2.5rem;
+        padding: 0;
+        margin: 0;
+        margin-left: 10px;
+        margin-bottom: 15px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    button.open-btn:hover {
+        background-color: #131313;
+    }
+
+
     ul {
         padding: 0;
         list-style: none;
@@ -162,7 +222,7 @@
     }
 
     li {
-        padding: .8em;
+        padding: 0.8em;
         border-bottom: 1px solid #444;
         background: #333;
     }
--- frontend/src/views/Certs.svelte
+++ frontend/src/views/Certs.svelte
@@ -1,182 +1,244 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { GenerateKeys, GenerateSelfSignedCertificate } from "../../wailsjs/go/backend/Backend";
+    import {
+        GenerateKeys,
+        GenerateSelfSignedCertificate,
+        OpenFile,
+        OpenDir,
+    } from "../../wailsjs/go/backend/Backend";
     import { openDialog, closeDialog } from "../functions/functions";
     import CustomDialog from "../components/dialogs/CustomDialog.svelte";
-  
+    import openIcon from "../assets/images/open.png";
+
     let dialog = { showDialog: false, dialogTitle: "", dialogMessage: "" };
-  
-    let keyType = '';
-    let keyName = '';
-    let outputPath = '';
+
+    let keyType = "";
+    let keyName = "";
+    let outputPath = "";
     let overwrite = false;
-  
-    let privateKeyPath = '';
-  
+
+    let privateKeyPath = "";
+
     async function generateKeys() {
         if (!keyType || !keyName || !outputPath) {
             dialog = openDialog(dialog, "Error", "All fields are required");
             return;
         }
-  
+
         try {
             await GenerateKeys(keyType, keyName, outputPath, overwrite);
-            dialog = openDialog(
-                dialog,
-                "Success",
-                `Successfully created keys`
-            );
+            dialog = openDialog(dialog, "Success", `Successfully created keys`);
             keyType = "";
             keyName = "";
             outputPath = "";
             overwrite = false;
         } catch (err) {
+            dialog = openDialog(dialog, "Error", `${err}`);
+        }
+    }
+
+    async function generateCertificate() {
+        if (!privateKeyPath) {
             dialog = openDialog(
                 dialog,
                 "Error",
-                `${err}`
+                "Private key path is required"
             );
+            return;
+        }
+
+        try {
+            await GenerateSelfSignedCertificate(privateKeyPath);
+            dialog = openDialog(
+                dialog,
+                "Success",
+                `Successfully created self-signed certificate`
+            );
+            privateKeyPath = "";
+        } catch (err) {
+            dialog = openDialog(dialog, "Error", `${err}`);
         }
     }
-  
-    async function generateCertificate() {
-          if (!privateKeyPath) {
-              dialog = openDialog(dialog, "Error", "Private key path is required");
-              return;
-          }
-  
-          try {
-              await GenerateSelfSignedCertificate(privateKeyPath);
-              dialog = openDialog(
-                  dialog,
-                  "Success",
-                  `Successfully created self-signed certificate`
-              );
-              privateKeyPath = "";
-          } catch (err) {
-              dialog = openDialog(
-                  dialog,
-                  "Error",
-                  `${err}`
-              );
-          }
-      }
-  </script>
-  
-  <CustomDialog
-      bind:show={dialog.showDialog}
-      title={dialog.dialogTitle}
-      message={dialog.dialogMessage}
-      onClose={() => (dialog = closeDialog(dialog))}
-      confirmButton={false}
-  />
-  
-  <div class="form-container">
-      <h2>Generate Keys</h2>
-  <form on:submit|preventDefault={generateKeys}>
-      <label class="input-field">
-          <span>Key Type:</span>
-          <select bind:value={keyType}>
-              <option value=''>--Please choose an option--</option>
-              <option>rsa</option>
-              <option>dsa</option>
-              <option>ecdsa</option>
-              <option>ed25519</option>
-          </select>
-      </label>
-  
-      <label class="input-field">
-          <span>Key Name:</span>
-          <input type="text" bind:value={keyName} />
-      </label>
-  
-      <label class="input-field">
-          <span>Output Path:</span>
-          <input type="text" bind:value={outputPath} />
-      </label>
-  
-      <label class="input-field checkbox-field">
-          <span>Overwrite existing keys?</span>
-          <select bind:value={overwrite}>
-              <option value={true}>Yes</option>
-              <option value={false}>No</option>
-          </select>
-      </label>
-  
-      <button class="submit-button" type="submit">Generate Keys</button>
-  </form>
-  </div>
-  
-  <hr>
-  
-  <div class="form-container">
-      <h2>Generate Self-Signed Certificate</h2>
-  <form on:submit|preventDefault={generateCertificate}>
-    <label class="input-field">
-        <span>Private Key Path:</span>
-        <input type="text" bind:value={privateKeyPath} />
-    </label>
-  
-    <button class="submit-button" type="submit">Generate Self-Signed Certificate</button>
-  </form>
-  </div>
-  
-  <style>
-      .form-container {
-          width: 90%;
-          max-width: 600px;
-          margin: 3rem auto;
-          padding: 2rem;
-          background: #282828;
-          box-shadow: 0 0 15px rgba(0,0,0,0.3);
-          border-radius: 5px;
-      }
-  
-      .input-field, .checkbox-field {
-          margin-bottom: 1.5rem;
-      }
-  
-      .input-field > span, .checkbox-field > span {
-          font-weight: 500;
-          color: #ddd;
-          margin-bottom: .5rem;
-      }
-  
-      .input-field > input, .input-field > select {
-          padding: .7em;
-          border: 0;
-          border-radius: 4px;
-          background: #333;
-          color: #fff;
-      }
-  
-      .checkbox-field {
-          display: flex;
-          align-items: center;
-          color: #ddd;
-      }
-  
-      .submit-button {
-          padding: .8em 1em;
-          border: none;
-          border-radius: 4px;
-          background: #1abc9c;
-          color: #fff;
-          cursor: pointer;
-          transition: background-color 0.3s;
-      }
-  
-      .submit-button:hover {
-          background-color: #16a085;
-      }
-  
-      hr {
-          border: 0;
-          height: 1px;
-          background: #444;
-      }
-  
-      h2 {
-          color: white;
-      }
-  </style>
+
+    const selectFile = async () => {
+        const file = await OpenFile();
+        privateKeyPath = file;
+    };
+
+    const selectDir = async () => {
+        const dir = await OpenDir();
+        outputPath = dir;
+    };
+</script>
+
+<CustomDialog
+    bind:show={dialog.showDialog}
+    title={dialog.dialogTitle}
+    message={dialog.dialogMessage}
+    onClose={() => (dialog = closeDialog(dialog))}
+    confirmButton={false}
+/>
+
+<div class="form-container">
+    <h2>Generate Keys</h2>
+    <form on:submit|preventDefault={generateKeys}>
+        <label class="input-field">
+            <span>Key Type:</span>
+            <select bind:value={keyType}>
+                <option value="">--Please choose an option--</option>
+                <option>rsa</option>
+                <option>dsa</option>
+                <option>ecdsa</option>
+                <option>ed25519</option>
+            </select>
+        </label>
+
+        <label class="input-field">
+            <span>Key Name:</span>
+            <input type="text" bind:value={keyName} />
+        </label>
+
+        <label class="input-field">
+            <span>Output Path:</span>
+            <div class="input-group">
+                <input type="text" bind:value={outputPath} />
+                <button
+                    type="button"
+                    class="open-btn"
+                    title="Select Output Directory"
+                    on:click={selectDir}
+                >
+                    <img src={openIcon} alt="Open Dir" class="open-icon" />
+                </button>
+            </div>
+        </label>
+
+        <label class="input-field checkbox-field">
+            <span>Overwrite existing keys?</span>
+            <select bind:value={overwrite}>
+                <option value={true}>Yes</option>
+                <option value={false}>No</option>
+            </select>
+        </label>
+
+        <button class="submit-button" type="submit">Generate Keys</button>
+    </form>
+</div>
+
+<hr />
+
+<div class="form-container">
+    <h2>Generate Self-Signed Certificate</h2>
+    <form on:submit|preventDefault={generateCertificate}>
+        <label class="input-field">
+            <span>Private Key Path:</span>
+            <div class="input-group">
+                <input type="text" bind:value={privateKeyPath} />
+                <button
+                    type="button"
+                    class="open-btn"
+                    title="Select Private Key"
+                    on:click={selectFile}
+                >
+                    <img src={openIcon} alt="Open File" class="open-icon" />
+                </button>
+            </div>
+        </label>
+
+        <button class="submit-button" type="submit"
+            >Generate Self-Signed Certificate</button
+        >
+    </form>
+</div>
+
+<style>
+    .form-container {
+        width: 90%;
+        max-width: 600px;
+        margin: 3rem auto;
+        padding: 2rem;
+        background: #282828;
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
+        border-radius: 5px;
+    }
+
+    .input-field,
+    .checkbox-field {
+        margin-bottom: 1.5rem;
+    }
+
+    .input-field > span,
+    .checkbox-field > span {
+        font-weight: 500;
+        color: #ddd;
+        margin-bottom: 0.5rem;
+    }
+
+    .input-field > input,
+    .input-field > select {
+        padding: 0.7em;
+        border: 0;
+        border-radius: 4px;
+        background: #333;
+        color: #fff;
+    }
+
+    .checkbox-field {
+        display: flex;
+        align-items: center;
+        color: #ddd;
+    }
+
+    .submit-button {
+        padding: 0.8em 1em;
+        border: none;
+        border-radius: 4px;
+        background: #1abc9c;
+        color: #fff;
+        cursor: pointer;
+        transition: background-color 0.3s;
+    }
+
+    .submit-button:hover {
+        background-color: #16a085;
+    }
+
+    button.open-btn {
+        background: none;
+        width: 2.5rem;
+        height: 2.5rem;
+        padding: 0;
+        margin: 0;
+        margin-left: 10px;
+        margin-bottom: 15px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    button.open-btn:hover {
+        background-color: #131313;
+    }
+
+    .input-group {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        margin-bottom: 1em;
+    }
+
+    .input-group input {
+        margin-right: 0.5em;
+        background: #333;
+    }
+
+    hr {
+        border: 0;
+        height: 1px;
+        background: #444;
+    }
+
+    h2 {
+        color: white;
+    }
+</style>
