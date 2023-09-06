014ea55ef6a4b8a609763275deee861ad67a6308
Miguel Fernandez <miguelfdez99@gmail.com>
2023-09-06 18:22:49 +0200

    added timeout

--- frontend/src/views/Logs.svelte
+++ frontend/src/views/Logs.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
     import { onMount, onDestroy } from "svelte";
-    import { GetLogs, ExportLogs, OpenFile, OpenDialogInfo, OpenDialogError } from "../../wailsjs/go/backend/Backend";
+    import {
+        GetLogs,
+        ExportLogs,
+        OpenFile,
+        OpenDialogInfo,
+        OpenDialogError,
+    } from "../../wailsjs/go/backend/Backend";
     import { writable } from "svelte/store";
     import openIcon from "../assets/images/open.png";
-    import { settings } from '../stores';
+    import { settings } from "../stores";
 
     let fontSize: string;
     let color: string;
@@ -23,13 +29,31 @@
     });
 
     $: {
-    document.documentElement.style.setProperty('--main-font-size', fontSize);
-    document.documentElement.style.setProperty('--main-color', color);
-    document.documentElement.style.setProperty('--main-font-family', fontFamily);
-    document.documentElement.style.setProperty('--main-bg-color', backgroundColor);
-    document.documentElement.style.setProperty('--main-bg-color2', backgroundColor2);
-    document.documentElement.style.setProperty('--main-input-color', inputColor);
-    document.documentElement.style.setProperty('--main-button-color', buttonColor);
+        document.documentElement.style.setProperty(
+            "--main-font-size",
+            fontSize
+        );
+        document.documentElement.style.setProperty("--main-color", color);
+        document.documentElement.style.setProperty(
+            "--main-font-family",
+            fontFamily
+        );
+        document.documentElement.style.setProperty(
+            "--main-bg-color",
+            backgroundColor
+        );
+        document.documentElement.style.setProperty(
+            "--main-bg-color2",
+            backgroundColor2
+        );
+        document.documentElement.style.setProperty(
+            "--main-input-color",
+            inputColor
+        );
+        document.documentElement.style.setProperty(
+            "--main-button-color",
+            buttonColor
+        );
     }
 
     let filepath: string = "";
@@ -40,20 +64,32 @@
     let bootNumber = writable(0);
     const ActiveComponent = { NONE: 0, SEARCH: 1, EXPORT: 2, BOOT_NUMBER: 3 };
     let activeComponent = ActiveComponent.NONE;
+    let fetchTimeout: number;
 
     let fetchLogs = async (type, boot) => {
-        loading.set(true);
-        try {
-            const result = await GetLogs(type, boot);
-            //const cleanedLogs = result.map(log => log.split(' ').slice(4).join(' ')); 
-            logs.set(result.slice().reverse()); 
-        } catch (error) {
-            await OpenDialogError(`Failed to fetch logs: ${error}`);
-        } finally {
-            loading.set(false);
+        if (fetchTimeout) {
+            clearTimeout(fetchTimeout);
         }
-};
 
+        fetchTimeout = setTimeout(async () => {
+            loading.set(true);
+            try {
+                const result = await GetLogs(type, boot);
+                //const cleanedLogs = result.map(log => log.split(' ').slice(4).join(' '));
+                logs.set(result.slice().reverse());
+            } catch (error) {
+                if (error.includes("Invalid boot number")) {
+                    await OpenDialogError(
+                        `The provided boot number is not valid: ${boot}`
+                    );
+                } else {
+                    await OpenDialogError(`Failed to fetch logs: ${error}`);
+                }
+            } finally {
+                loading.set(false);
+            }
+        }, 300);
+    };
 
     let exportLogs = async () => {
         loading.set(true);
@@ -100,15 +136,27 @@
 <div class="cont">
     <div class="control-buttons">
         <button
-            on:click={() => activeComponent = activeComponent !== ActiveComponent.SEARCH ? ActiveComponent.SEARCH : ActiveComponent.NONE}
+            on:click={() =>
+                (activeComponent =
+                    activeComponent !== ActiveComponent.SEARCH
+                        ? ActiveComponent.SEARCH
+                        : ActiveComponent.NONE)}
             title="Toggle search">🔍</button
         >
         <button
-            on:click={() => activeComponent = activeComponent !== ActiveComponent.EXPORT ? ActiveComponent.EXPORT : ActiveComponent.NONE}
+            on:click={() =>
+                (activeComponent =
+                    activeComponent !== ActiveComponent.EXPORT
+                        ? ActiveComponent.EXPORT
+                        : ActiveComponent.NONE)}
             title="Toggle export">💾</button
         >
         <button
-            on:click={() => activeComponent = activeComponent !== ActiveComponent.BOOT_NUMBER ? ActiveComponent.BOOT_NUMBER : ActiveComponent.NONE}
+            on:click={() =>
+                (activeComponent =
+                    activeComponent !== ActiveComponent.BOOT_NUMBER
+                        ? ActiveComponent.BOOT_NUMBER
+                        : ActiveComponent.NONE)}
             title="Toggle boot number">🔢</button
         >
     </div>
@@ -116,28 +164,36 @@
     <h1>Log viewer</h1>
 
     {#if activeComponent === ActiveComponent.SEARCH}
-    <label >
-        <p>Search:</p>
-        <input bind:value={$searchString} placeholder="Search logs..." />
-    </label>
+        <label>
+            <p>Search:</p>
+            <input bind:value={$searchString} placeholder="Search logs..." />
+        </label>
     {/if}
 
     {#if activeComponent === ActiveComponent.EXPORT}
-    <label>
-        <p>Export:</p>
-        <div class="input-group">
-        <input
-                type="text"
-                bind:value={filepath}
-                placeholder="Enter a file path"
-            />
-        <button class="open-btn" title="Choose File" on:click={selectFile}
-                ><img src={openIcon} alt="Open Dir" class="open-icon" /></button
+        <label>
+            <p>Export:</p>
+            <div class="input-group">
+                <input
+                    type="text"
+                    bind:value={filepath}
+                    placeholder="Enter a file path"
+                />
+                <button
+                    class="open-btn"
+                    title="Choose File"
+                    on:click={selectFile}
+                    ><img
+                        src={openIcon}
+                        alt="Open Dir"
+                        class="open-icon"
+                    /></button
+                >
+            </div>
+            <button class="export-button" on:click={exportLogs}
+                >Export Logs</button
             >
-        </div>
-        <button class="export-button" on:click={exportLogs}>Export Logs</button>
-    </label>
-    
+        </label>
     {/if}
 
     {#if activeComponent === ActiveComponent.BOOT_NUMBER}
@@ -282,7 +338,7 @@
 
     .spinner {
         border: 4px solid rgba(255, 255, 255, 0.3);
-        border-top: 4px solid var(--main-color);;
+        border-top: 4px solid var(--main-color);
         border-radius: 50%;
         width: 30px;
         height: 30px;
